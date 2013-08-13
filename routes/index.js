617bb4002b2e4fe0f7f1f5db6e030df34de913a3
Fernando Sanz <correo@fernando.com.ar>
2013-08-13 18:34:53 +0200

    video, galeria, validacion

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,26 @@ module.exports = function(app) {
     return (user.is_admin || user.id === project.leader.id);
   };
 
+  app.locals.getVideoEmbed = function(string) {
+	vimeo_regex=/^http:\/\/vimeo.com\/([0-9\.]+)$/
+    vimeo_r=string.match(vimeo_regex);
+	if(vimeo_r) return('http://player.vimeo.com/video/'+vimeo_r[1]);
+	youtube_regex=/^http:\/\/www.youtube.com\/watch\?v=([A-Za-z0-9\.]+)$/
+    youtube_r=string.match(youtube_regex);
+	if(youtube_r) return('http://www.youtube.com/embed/'+youtube_r[1]);
+	youtube_regex=/^http:\/\/youtu.be\/([A-Za-z0-9\.]+)$/
+    youtube_r=string.match(youtube_regex);
+	if(youtube_r) return('http://www.youtube.com/embed/'+youtube_r[1]);
+	return false;
+  };  
+  
+  app.locals.getGalleryEmbed = function(string) {
+	imgur_regex=/^http:\/\/imgur.com\/a\/([A-Za-z0-9\.]+)$/
+    imgur_r=string.match(imgur_regex);
+	if(imgur_r) return('http://imgur.com/a/'+imgur_r[1]);
+	return false;
+  };
+
   require('./site')(app);
   require('./api')(app);
   require('./admin')(app);
